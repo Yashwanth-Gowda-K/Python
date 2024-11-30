0b783a372c09cf68c85cd7f2033236c8ec8b295e
Yashavanth Gowda K K <yashyash808859@gmail.com>
2024-11-30 12:57:52 +0530

    Add files via upload

---
+++ measur.py
@@ -0,0 +1,21 @@
+  # Import the time module to measure execution time
+
+# Prompt the user to enter the number of iterations for the loop
+  # Record the start time
+
+# Execute the loop for the given number of iterations
+  # A simple loop for demonstration
+  # Record the end time
+
+# Calculate and print the execution time
+
+import time
+iterations = int(input("Enter the number to iterations: "))
+start_time = time.time()
+
+for i in range(iterations):
+    pass
+
+end_time = time.time()
+
+print(f"Execution Time: {end_time - start_time:.2f}seconds")
