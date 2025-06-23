0305cbc91c28887ee69fca95bad4df6b67287990
Yashavanth  K K <yashyash808859@gmail.com>
2025-06-23 21:55:41 +0530

    Update Measure Execution Time.py

--- Measure Execution Time.py
+++ Measure Execution Time.py
@@ -1,16 +1,6 @@
-  # Import the time module to measure execution time
-
-# Prompt the user to enter the number of iterations for the loop
-  # Record the start time
-
-# Execute the loop for the given number of iterations
-  # A simple loop for demonstration
-  # Record the end time
-
-# Calculate and print the execution time
-
 import time
-iterations = int(input("Enter the number to iterations: "))
+
+iterations = int(input("Enter the number of iterations: "))
 start_time = time.time()
 
 for i in range(iterations):
@@ -18,4 +8,4 @@ for i in range(iterations):
 
 end_time = time.time()
 
-print(f"Execution Time: {end_time - start_time:.2f}seconds")
+print(f"Execution Time: {end_time - start_time:.6f} seconds")
