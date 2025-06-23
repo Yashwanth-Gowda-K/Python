bfc2ed1ecbd2ecab3eab51d91be29287e5a2fe56
Yashavanth  K K <yashyash808859@gmail.com>
2025-06-23 21:31:38 +0530

    Update Largest of Three Numbers.py

--- Largest of Three Numbers.py
+++ Largest of Three Numbers.py
@@ -1,10 +1,34 @@
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
-c = float(input("Enter the thrid number: "))
+def get_valid_number(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Error: Invalid input. Try again.")
 
-if a>= b and a>=c:
-    print(f"{a} is the largest number..")
-elif b>=a and b>= c:
-    print(f"{b} is the largest number...")
-else:
-    print(f"{c} is the largest number...")
+def find_largest():
+    print("\n" + "=" * 40)
+    print("Enter 3 numbers to find the largest".center(40))
+    print("=" * 40)
+
+    a = get_valid_number("Enter the 1st number: ")
+    b = get_valid_number("Enter the 2nd number: ")
+    c = get_valid_number("Enter the 3rd number: ")
+
+    if a >= b and a >= c:
+        print(f"{a} is the greatest number.")
+    elif b >= a and b >= c:
+        print(f"{b} is the greatest number.")
+    else:
+        print(f"{c} is the greatest number.")
+
+def main():
+    while True:
+        find_largest()
+        play_again = input("Do you want to play again? (y/n): ").lower()
+
+        if play_again != 'y':
+            print("Thanks for playing!")
+            break
+
+if __name__ == '__main__':
+    main()
